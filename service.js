49cc23c12b9e9f81dab1f27d37bd48e749c91476
George Lopez-Ayala <georgelopezayala@icloud.com>
2017-01-19 12:13:49 -0700

    d

--- service.js
+++ service.js
@@ -1,10 +1,19 @@
-angular.module('userProfiles').service('mainService', function($http) {
-
+angular.module('userProfiles').service('mainService', function($http, $q) {
   this.getUsers = function() {
-    return $http({
+    var deferred = $q.defer();
+    $http({
         method: 'GET',
-        url: 'http://reqres.in/api/users?page=1'
+        url: 'https://reqres.in/api/users?page=1'
+    }).then(function(response) {
+      var parsedResponse = response.data.data;
+      for(var i = 0; i < parsedResponse.length; i++) {
+        parsedResponse[i].first_name = 'Pablo';
+        parsedResponse[0].last_name = 'Corona';
+        parsedResponse[1].last_name = 'Montero';
+        parsedResponse[2].last_name = 'Santos'
+      }
+      deferred.resolve(parsedResponse)
     })
+    return deferred.promise;
   }
-
 });
